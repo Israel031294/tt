5e18063282f9c98e2a995a55b62e4e07ee16e7e1
Israel <wy_darkmagician@hotmail.com>
2021-11-25 23:13:47 -0600

    up
    up

--- index.php
+++ index.php
@@ -9,13 +9,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<<<<<<< HEAD
   
 	
  
+=======
+       
+
+>>>>>>> israel
 </head>
 <body>
     <header>
         
+<<<<<<< HEAD
     <nav class="indigo">
         <div class="nav-wrapper container">
         <a href="#" class="brand-logo"><i class="fas fa-home fa-2x"></i></a>
@@ -23,12 +29,31 @@
             <li><a href="#">Conocenos</a></li>
             <li><a href="./evaluacion/login.html">Log In</a></li>
             <li><a href="./evaluacion/cuenta/crearCuenta.html">Crear una cuenta</a></li>
+=======
+    <div class="navbar-fixed">
+    <nav class="light-green darken-2">
+      <div class="nav-wrapper container">
+        <a href="#!" class="brand-logo"><i class="fas fa-home fa-2x"></i></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fas fa-bars"></i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="">Conocenos</a></li>
+          <li><a href="./inicio/login.html">Log In</a></li>
+          <li><a href="./inicio/crearCuenta.html">Crear cuenta</a></li>
+>>>>>>> israel
         </ul>
-        </div>
-    </nav>
+      </div>
+    </nav> <!-- /menu -->
+    </div>
+    <ul class="sidenav" id="mobile-demo">
+          <li><a href="">Conocenos</a></li>
+          <li><a href="./inicio/login.html">Log In</a></li>
+          <li><a href="./inicio/crearCuenta.html">Crear cuenta</a></li>
+    </ul> <!-- /menu mobile-->
 
     </header>
+    <!--main-->
 
+<<<<<<< HEAD
 
     <section>
         <div class="container">
@@ -38,30 +63,75 @@
     </section>
 
         
+=======
+    <!--slider-->
+    <div class="slider">
+        <ul class="slides">
+          <li>
+            <img src="./assets/img/inicio.jpg">
+            <div class="caption left-align">
+              <h3>Bienvenido!!</h3>
+              <h5 class="light grey-text text-lighten-3">Evalua la viabilidad de tu proyecto</h5>
+            </div>
+          </li>
+          <li>
+            <img src="./assets/img/inicio2.jpg">
+            <div class="caption left-align">
+              <h3>¿Qué es un Proyecto de inversión?</h3>
+              <h5 class="light grey-text text-lighten-3"></h5>
+            </div>
+          </li>
+          <li>
+            <img src="./assets/img/inicio4.jpg"> 
+            <div class="caption right-align">
+              <h3>cCuánto dinero necesito para arrancar el proyecto?
+            </h3>
+            </div>
+          </li>
+          <li>
+            <img src="./assets/img/inicio3.jpg"> 
+            <div class="caption right-align">
+              <h3>¿En que tiempo se recuperará la inversión?</h3>
+            </div>
+          </li>
+          <li>
+            <img src="./assets/img/ESCOM_1.jpg">
+            <div class="caption center-align">
+              <h3>Una propuesta politécnica!!</h3>
+              <h5 class="light grey-text text-lighten-3"></h5>
+            </div>
+          </li>
+        </ul>
+        <!--fin slider-->
+    
+    <!--fin main-->
+>>>>>>> israel
 
-   
-    <footer class="page-footer black">
+    <footer class="page-footer grey darken-4">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 class="white-text">Trabajo Terminal 2021-A057</h5>
+                <p class="grey-text text-lighten-4">Escuela Superior de Cómputo - IPN</p>
               </div>
               <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-university fa-2x"></i></a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-graduation-cap fa-2x"></i></a></li>
+          
                 </ul>
               </div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
+<<<<<<< HEAD
             © 2021 Copyright Text
             <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+=======
+            © 2021 Copyright
+>>>>>>> israel
             </div>
           </div>
         </footer>
@@ -70,6 +140,7 @@
 
 
 <!------- JavaScript ---->
+<<<<<<< HEAD
 <!--<script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 -->
@@ -77,7 +148,15 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 
+=======
 
-    
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="./js/index.js"></script>
+
+ 
+>>>>>>> israel
+
 </body>
 </html>
